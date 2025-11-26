659b310e4f2f9c529caf8c15fa075fd1243eb13e
flynnZ0 <10cantilabryanjames121003@gmail.com>
2025-11-26 16:39:45 +0800

    test

--- app/Providers/AssociateProvider.php
+++ app/Providers/AssociateProvider.php
@@ -21,8 +21,15 @@ class AssociateProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        View::composer(['pages.waitlist.modals.add', 'pages.patients.modals.waitlist-add', 'pages.appointments.index', 'pages.recalls.modals.add', 'pages.recalls.modals.edit'], function ($view) {
+        View::composer(['pages.waitlist.modals.add', 'pages.patients.modals.waitlist-add', 'pages.recalls.modals.add', 'pages.recalls.modals.edit'], function ($view) {
             $view->with('associates', Associate::where('is_active', true)->get());
+        });
+
+        View::composer(['pages.appointments.index'], function ($view) {
+            $activeClinicId = session('clinic_id');
+            $view->with('associates', Associate::where('is_active', true)
+                ->where('clinic_id', $activeClinicId)
+                ->get());
         });
     }
 }
