ea03e70c4feab85e5382d59d86abf5c2a862c1b9
flynnZ0 <10cantilabryanjames121003@gmail.com>
2025-11-30 13:48:35 +0800

    try

--- resources/views/pages/staffs/modals/edit.blade.php
+++ resources/views/pages/staffs/modals/edit.blade.php
@@ -119,7 +119,7 @@
 
                 <!-- Footer -->
                 <div class="modal-footer">
-                    <button type="submit" class="btn btn-primary">💾 Update Staff</button>
+                    <button type="submit" class="btn btn-primary"><i class="bi bi-save me-2">Update Staff</button>
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><i class="bi bi-x-lg me-2"></i>Close</button>
                 </div>
             </form>
